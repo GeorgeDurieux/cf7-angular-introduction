deacec5e673d0bdbb0c9a5b49bcd44ee5d5072a9
George Durieux <giorgosdurieux@gmail.com>
2025-05-02 17:51:04 +0300

    sort

--- src/app/components/simple-datatable/simple-datatable.component.ts
+++ src/app/components/simple-datatable/simple-datatable.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { EPerson } from '../../shared/interfaces/eperson';
+import { sortBy } from 'lodash-es'
 
 @Component({
   selector: 'app-simple-datatable',
@@ -9,4 +10,16 @@ import { EPerson } from '../../shared/interfaces/eperson';
 })
 export class SimpleDatatableComponent {
     @Input() data: EPerson[] | undefined
+
+    sortOrder = {
+        givenName: 'none',
+        surname: 'none',
+        age: 'none',
+        email: 'none',
+        education: 'none'
+    }
+
+    onPersonClicked(person: EPerson) {
+        console.log(person)
+    }
 }
