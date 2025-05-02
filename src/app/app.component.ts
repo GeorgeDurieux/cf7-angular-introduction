4dd94a788cd751618e234cfefd1d7a94f4c3d34f
George Durieux <giorgosdurieux@gmail.com>
2025-05-02 10:54:38 +0300

    new component: person-table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PersonTableComponent } from './components/person-table/person-table.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [PersonTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-introduction';
+  name = 'George'
+
+  person = {
+    givenName: 'George',
+    surname: 'Durieux',
+    age: '33',
+    email: 'giorgosdurieux@aueb.gr'
+  }
 }
