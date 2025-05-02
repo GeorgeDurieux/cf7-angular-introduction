0d3ba474353afeaaf9d29835c30c12b10b97f564
George Durieux <giorgosdurieux@gmail.com>
2025-05-02 15:47:23 +0300

    for directive

--- src/app/components/component-input-example/component-input-example.component.ts
+++ src/app/components/component-input-example/component-input-example.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { Person } from '../../shared/interfaces/person';
+import { PersonTableComponent } from '../person-table/person-table.component';
+
 
 @Component({
   selector: 'app-component-input-example',
-  imports: [],
+  imports: [PersonTableComponent],
   templateUrl: './component-input-example.component.html',
   styleUrl: './component-input-example.component.css'
 })
 export class ComponentInputExampleComponent {
+    person0: Person = {
+        givenName: 'Jason',
+        surname: 'Momoa',
+        age: 35,
+        email: 'khal@drogo.com',
+        address: 'World, Wide'
+    }
 
+    person1: Person = {
+        givenName: 'John',
+        surname: 'Doe',
+        age: 25,
+        email: 'lol@example.com',
+        address: 'Athens, Greece'
+    }
 }
--- src/app/components/for-directive-example/for-directive-example.component.ts
+++ src/app/components/for-directive-example/for-directive-example.component.ts
@@ -1,11 +1,135 @@
 import { Component } from '@angular/core';
+import { Person } from '../../shared/interfaces/person';
+import { PersonTableComponent } from "../person-table/person-table.component";
 
 @Component({
-  selector: 'app-for-directive-example',
-  imports: [],
-  templateUrl: './for-directive-example.component.html',
-  styleUrl: './for-directive-example.component.css'
+    selector: 'app-for-directive-example',
+    imports: [PersonTableComponent],
+    templateUrl: './for-directive-example.component.html',
+    styleUrl: './for-directive-example.component.css'
 })
 export class ForDirectiveExampleComponent {
-
+    users: Person[] = [
+        {
+            "givenName": "Hadleigh",
+            "surname": "Iskower",
+            "age": 1,
+            "email": "hiskower0@youku.com",
+            "address": "Room 1670"
+        }, {
+            "givenName": "Myrtice",
+            "surname": "Warner",
+            "age": 2,
+            "email": "mwarner1@amazon.com",
+            "address": "Suite 55"
+        }, {
+            "givenName": "Merna",
+            "surname": "Canas",
+            "age": 3,
+            "email": "mcanas2@washingtonpost.com",
+            "address": "PO Box 36519"
+        }, {
+            "givenName": "Marlie",
+            "surname": "Gook",
+            "age": 4,
+            "email": "mgook3@hostgator.com",
+            "address": "Apt 1138"
+        }, {
+            "givenName": "Merell",
+            "surname": "Exon",
+            "age": 5,
+            "email": "mexon4@npr.org",
+            "address": "Room 1226"
+        }, {
+            "givenName": "Lyon",
+            "surname": "Sutter",
+            "age": 6,
+            "email": "lsutter5@arstechnica.com",
+            "address": "Suite 82"
+        }, {
+            "givenName": "Virge",
+            "surname": "Minors",
+            "age": 7,
+            "email": "vminors6@i2i.jp",
+            "address": "Suite 49"
+        }, {
+            "givenName": "Melicent",
+            "surname": "Aleksandrov",
+            "age": 8,
+            "email": "maleksandrov7@newsvine.com",
+            "address": "Suite 44"
+        }, {
+            "givenName": "Mindy",
+            "surname": "MacPhaden",
+            "age": 9,
+            "email": "mmacphaden8@wp.com",
+            "address": "Suite 75"
+        }, {
+            "givenName": "Milty",
+            "surname": "Gerholz",
+            "age": 10,
+            "email": "mgerholz9@pinterest.com",
+            "address": "Suite 74"
+        }, {
+            "givenName": "Emmeline",
+            "surname": "Paxforde",
+            "age": 11,
+            "email": "epaxfordea@archive.org",
+            "address": "Room 411"
+        }, {
+            "givenName": "Janessa",
+            "surname": "Clay",
+            "age": 12,
+            "email": "jclayb@mail.ru",
+            "address": "Room 650"
+        }, {
+            "givenName": "Fey",
+            "surname": "le Keux",
+            "age": 13,
+            "email": "flekeuxc@gizmodo.com",
+            "address": "PO Box 69223"
+        }, {
+            "givenName": "Erhart",
+            "surname": "Glasscoo",
+            "age": 14,
+            "email": "eglasscood@howstuffworks.com",
+            "address": "Apt 1164"
+        }, {
+            "givenName": "Kelsi",
+            "surname": "Rodbourne",
+            "age": 15,
+            "email": "krodbournee@pen.io",
+            "address": "Room 12"
+        }, {
+            "givenName": "Tammi",
+            "surname": "Andreaccio",
+            "age": 16,
+            "email": "tandreacciof@time.com",
+            "address": "Apt 521"
+        }, {
+            "givenName": "Sharlene",
+            "surname": "Farny",
+            "age": 17,
+            "email": "sfarnyg@w3.org",
+            "address": "Apt 525"
+        }, {
+            "givenName": "Dicky",
+            "surname": "Josselsohn",
+            "age": 18,
+            "email": "djosselsohnh@clickbank.net",
+            "address": "Suite 6"
+        }, {
+            "givenName": "Christye",
+            "surname": "Kordas",
+            "age": 19,
+            "email": "ckordasi@auda.org.au",
+            "address": "PO Box 13050"
+        }, {
+            "givenName": "Herbie",
+            "surname": "Tiuit",
+            "age": 20,
+            "email": "htiuitj@vkontakte.ru",
+            "address": "20th Floor"
+        }
+    ]
 }
