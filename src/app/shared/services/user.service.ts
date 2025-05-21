6a584fa9531f46354073a7ebfd940bed1736ebef
George Durieux <giorgosdurieux@gmail.com>
2025-05-21 13:39:14 +0300

    registration form

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment.development';
+import { User } from '../interfaces/user';
+
+const API_URL = `${environment.apiURL}/api/users`
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+    http: HttpClient = inject(HttpClient)
+
+    registerUser(user: User) {
+        return this.http.post<{status: boolean, data: User}>(
+            `${API_URL}`, user
+        )
+    }
 }
